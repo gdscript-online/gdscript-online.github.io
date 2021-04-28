ba2e726683e050a51a0439b8ee2e11ae67981c38
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-04-28 15:34:41 +0200

    Display the engine version in the top-right corner

--- main_window.gd
+++ main_window.gd
@@ -3,7 +3,16 @@
 
 extends Control
 
+onready var engine_version_label := $EngineVersion as Label
+
 func _ready() -> void:
+	var version := Engine.get_version_info()
+	# Mimic the official version numbering.
+	if version.patch >= 1:
+		engine_version_label.text = "Godot %s.%s.%s" % [version.major, version.minor, version.patch]
+	else:
+		engine_version_label.text = "Godot %s.%s" % [version.major, version.minor]
+
 	# Upscale everything if the display requires it (crude hiDPI support).
 	# This prevents UI elements from being too small on hiDPI displays.
 	if OS.get_screen_dpi() >= 192 and OS.get_screen_size().x >= 2048:
